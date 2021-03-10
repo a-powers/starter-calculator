f61cd7c3a1677eff472b9956ffb23de97f99ded8
apowers2 <awpowers@gmail.com>
2021-03-10 19:09:32 +0000

    update

--- main.py
+++ main.py
@@ -22,8 +22,12 @@ def calculations():
     first_num = int(input("What is your first number? "))
     for i in operation:
         print(i)
-    ops = input("What op do you want to make?")
     calc_ops = True
 
     while still_calculating:
-        
+        ops = input("What op do you want to make?")
+
+        second_num = int(input("What is the second number? "))
+
+
+calculations()        
