c0e3845d7f48449555a908468ca44287ae7b762a
a-powers <awpowers@gmail.com>
2021-03-13 19:22:43 +0000

    update

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ def mult(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
+symbol_dict = {
+    "+": add,
+    "-": sub,
+    "*": mult,
+    "/": divide
+
+}
+
+def calculations():
+    num1 = int(input("Type first number."))
+    for i in symbol_dict:
+        print(i)
+    symbol = input("Type math symbol")
+    num2 = int(input("Type second number"))
+    calc = symbol_dict[symbol]
+    
+        
+    
+calculations()
