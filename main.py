5b027a734a84921a9772d7222ada5ec92cb8a3f8
a-powers <awpowers@gmail.com>
2021-03-24 11:29:16 +0000

    update

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ calc_func = {
     "/":division
 }
 
+def calculator():
+    num1 = int(input("What is your first number? "))
+    for i in calc_func:
+        print(i)
+    func = input("Choose a math function. ")
+    continue_calculation = True
+
+    while continue_calculation:
+        num2 = int(input("What is your second number? "))
+        math_function = calc_func[func]
+        answer = math_function(num1, num2)
+        print(answer)
+        continue_calculation = False
+calculator()
